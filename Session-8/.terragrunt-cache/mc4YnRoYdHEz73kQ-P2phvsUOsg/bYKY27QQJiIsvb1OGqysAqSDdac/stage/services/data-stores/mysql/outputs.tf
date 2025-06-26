409a4bdd66e225db611b56e4127d64a68009037b
Abhiram Kuchibhotla <abhiram@cklabs.com>
2025-06-26 16:59:17 +0530

    removed the caches

--- Session-8/.terragrunt-cache/mc4YnRoYdHEz73kQ-P2phvsUOsg/bYKY27QQJiIsvb1OGqysAqSDdac/stage/services/data-stores/mysql/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "db_address" {
-  value = aws_db_instance.example.address
-}
-
-output "db_port" {
-  value = aws_db_instance.example.port
-}
--- Session-8/.terragrunt-cache/mc4YnRoYdHEz73kQ-P2phvsUOsg/bYKY27QQJiIsvb1OGqysAqSDdac/stage/services/webserver-cluster/main.tf
+++
@@ -1,7 +0,0 @@
-module "webserver-cluster" {
-  source = "../../../modules/services/webserver-cluster"
-
-  cluster_name = var.cluster_name
-  db_remote_state_bucket = var.db_remote_state_bucket
-  db_remote_state_key = var.db_remote_state_key
-}
